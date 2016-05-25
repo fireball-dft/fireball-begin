49accf805b5e7210696c679e32e244d0580fbddd
guilleaf <gufranco@mail.wvu.edu>
2016-05-25 18:05:18 -0400

    Creating symlinks and changing script to create them

--- src/runme_before_compile.sh
+++ src/runme_before_compile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+MASTER=fireball-master
+
 for i in a.GLOBAL b.FUNCTIONS c.SYSTEM f.MPI g.XC_FUNCTIONALS j.ASSEMBLERS p.HARRIS q.DOGS o.OUTPUT libs include Makefile MACHINES 
 do
-    if [ ! -e $i ]
+    if [ -e $i ]
     then
-	ln -s ../../fireball-thunder/src/$i
+	rm -rf $i
     fi
+    ln -s ../../${MASTER}/src/$i
 done
