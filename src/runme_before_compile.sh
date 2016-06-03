2a7aa90f1e11ce9c369a5a7c90230cadf0c1a4f6
Guillermo <gufranco@mail.wvu.edu>
2016-06-02 20:08:52 -0400

    Travis 2

--- src/runme_before_compile.sh
+++ src/runme_before_compile.sh
@@ -2,7 +2,7 @@
 
 MASTER=fireball-master
 
-for i in a.GLOBAL b.FUNCTIONS c.SYSTEM f.MPI g.XC_FUNCTIONALS j.ASSEMBLERS p.HARRIS q.DOGS o.OUTPUT libs include Makefile MACHINES 
+for i in a.GLOBAL b.FUNCTIONS c.SYSTEM f.MPI g.XC_FUNCTIONALS j.ASSEMBLERS p.HARRIS q.DOGS o.OUTPUT include Makefile MACHINES 
 do
     if [ -e $i ]
     then
@@ -10,3 +10,4 @@ do
     fi
     ln -s ../../${MASTER}/src/$i
 done
+mkdir libs
