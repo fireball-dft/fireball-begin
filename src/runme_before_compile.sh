f3cafbcd894b5394df57ca8e8081ca2dec4b1bae
guilleaf <gufranco@mail.wvu.edu>
2016-07-09 11:37:59 -0400

    Autoconfiscation of Fireball

--- src/runme_before_compile.sh
+++ src/runme_before_compile.sh
@@ -2,7 +2,7 @@
 
 MASTER=fireball-master
 
-for i in a.GLOBAL b.FUNCTIONS c.SYSTEM f.MPI g.XC_FUNCTIONALS j.ASSEMBLERS p.THEORY o.OUTPUT include Makefile MACHINES libs
+for i in a.GLOBAL b.FUNCTIONS c.SYSTEM f.MPI g.XC_FUNCTIONALS j.ASSEMBLERS p.THEORY o.OUTPUT include Makefile.in MACHINES libs
 do
     if [ -e $i ]
     then
